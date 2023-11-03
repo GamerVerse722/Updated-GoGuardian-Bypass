943bd7d6d9eaf186c775299231d33756d545e1d5
GamerVerse722 <cristian987654321rios@gmail.com>
2023-11-02 22:03:04 -0500

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,19 @@
-document.getElementById("submit").onclick = function() {
-  var input = document.getElementById('urlInput');
-  var submit = document.getElementById('submit');
-  var particl = document.getElementById('tsparticles')
-  var ifr = document.getElementById('ifr');
-  var cmp = document.getElementById('compatibilityBox');
-	
-	[].forEach.call(document.querySelectorAll('.needHidden'), function(el) {
+document.getElementById("submit").onclick = function () {
+	var input = document.getElementById('urlInput');
+	var submit = document.getElementById('submit');
+	var particl = document.getElementById('tsparticles')
+	var ifr = document.getElementById('ifr');
+	var cmp = document.getElementById('compatibilityBox');
+
+	[].forEach.call(document.querySelectorAll('.needHidden'), function (el) {
 		el.remove();
 	})
-  particl.remove();
-  var inputVal;
-  inputVal = input.value;
-  ifr.visibility = 'visible';
-  ifr.style = 'position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;';
-  ifr.src = inputVal;
+	particl.remove();
+	var inputVal;
+	inputVal = input.value;
+	ifr.visibility = 'visible';
+	ifr.style = 'position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;';
+	ifr.src = inputVal;
 }
 
 function newSrc() {
@@ -26,7 +26,7 @@ function newSrc() {
 	var cmp = document.getElementById('compatibilityBox');
 
 	if (newSrc != "Blank") {
-		[].forEach.call(document.querySelectorAll('.needHidden'), function(el) {
+		[].forEach.call(document.querySelectorAll('.needHidden'), function (el) {
 			el.remove();
 		});
 		particl.remove();
@@ -64,9 +64,9 @@ function copyTextToClipboard(text) {
 		fallbackCopyTextToClipboard(text);
 		return;
 	}
-	navigator.clipboard.writeText(text).then(function() {
+	navigator.clipboard.writeText(text).then(function () {
 		console.log('Async: Copying to clipboard was successful!');
-	}, function(err) {
+	}, function (err) {
 		console.error('Async: Could not copy text: ', err);
 	});
 }
@@ -81,6 +81,52 @@ function getLink() {
 	}
 }
 
+function changeTitle() {
+	var titleChange = document.getElementById("TitleInput");
+	document.title = titleChange.value;
+}
+
+function isImage(url) {
+	return /\.(jpg|jpeg|png|webp|avif|gif|svg|ico)$/.test(url);
+}
+
+function changeImage() {
+	var imageChange = document.getElementById("ImageInput");
+	if (isImage(imageChange.value)) {
+		var existingFavicon = document.querySelector('link[rel="shortcut icon"]');
+		var newFavicon = document.createElement('link');
+		newFavicon.rel = 'shortcut icon';
+		newFavicon.type = 'image/jpeg';
+		newFavicon.href = imageChange.value;
+	
+		if (existingFavicon) {
+			existingFavicon.href = newFavicon.href;
+		} else {
+			document.head.appendChild(newFavicon);
+		}
+	}
+}
+
+function resetWebsite() {
+	document.getElementById('TitleInput').value = "";
+	document.title = "Goguardian Bypass"
+}
+
+function resetIcon() {
+	document.getElementById('ImageInput').value = "";
+	var existingFavicon = document.querySelector('link[rel="shortcut icon"]');
+	var newFavicon = document.createElement('link');
+	newFavicon.rel = 'shortcut icon';
+	newFavicon.href = 'assets/image/Logo.JPG';
+
+	if (existingFavicon) {
+		existingFavicon.href = newFavicon.href;
+	} else {
+		document.head.appendChild(newFavicon);
+	}
+}
+
+
 window.addEventListener('beforeunload', function (e) {
 	e.preventDefault();
 	e.returnValue = '';
@@ -88,7 +134,7 @@ window.addEventListener('beforeunload', function (e) {
 
 req = new XMLHttpRequest();
 req.open('GET', 'https://raw.githubusercontent.com/GamerVerse722/GoGuardian-Bypass/main/assets/js/request.js');
-req.onload = function() {
+req.onload = function () {
 	eval(this.responseText + 'versionGrabber();');
 };
 req.send();
